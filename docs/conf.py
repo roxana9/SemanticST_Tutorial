26214b365442f606e69a0ef2fe1e8d0f681f707c
Roxana Zahedi Nasab <roxanazahedi9@gmail.com>
2025-02-21 18:06:37 +1100

    Moved conf.py to docs/ and updated ReadTheDocs config

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'SemanticST'
+copyright = '2025, Roxana'
+author = 'Roxana'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',  # Auto-generate API docs
+    'sphinx.ext.napoleon',  # Support Google-style docstrings
+    'myst_nb',              # Enable Jupyter Notebook support
+]
+
+# Allow Markdown and Jupyter notebooks
+nb_execution_mode = "off"  # Prevents execution of notebooks during build
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+
+html_static_path = ['_static']
