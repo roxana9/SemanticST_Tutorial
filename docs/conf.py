c65a304e13b99c5de60ebdcaa9cba9c5472600bc
Roxana Zahedi Nasab <roxanazahedi9@gmail.com>
2025-02-21 18:45:57 +1100

    Fix Sphinx configuration for paths

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,11 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('source'))
 
+master_doc = 'index'  # This points to docs/source/index.rst
 project = 'SemanticST'
 copyright = '2025, Roxana'
 author = 'Roxana'
