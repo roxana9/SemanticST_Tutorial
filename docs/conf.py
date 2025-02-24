8a98a75f8eab51a8f93069165b6654b87cc5be07
Roxana Zahedi Nasab <roxanazahedi9@gmail.com>
2025-02-24 14:27:47 +1100

    Update notebooks and move images to _static

--- docs/conf.py
+++ docs/conf.py
@@ -37,4 +37,4 @@ exclude_patterns = []
 #html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
 
-html_static_path = ['_static','notebooks/IMG_notebook']
+html_static_path = ['_static','source/notebooks/IMG_notebook']
